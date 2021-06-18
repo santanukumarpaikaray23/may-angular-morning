f100169f239dfd76f50343c8fc1ccdd422340278
kumar <santanupaikaray23@gmail.com>
2021-06-18 23:22:04 +0530

    add all changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-basic-advanced';
+  title = "santanu's";
+  name = "santanu kumar paikaray"
+
+  displayName(){
+    return this.name;
+  }
 }
